a7a03ddfb9e117be0bc0d68319935157a4f142da
lureiny <lureniy@gmail.com>
2020-02-07 15:31:06 +0800

    refactor(item.py): 增加必要注释

--- item.py
+++ item.py
@@ -8,18 +8,21 @@ class Item():
         super().__init__()
         self.__info = dict()        # Item的信息
     
+    # 更新存储对象类信息
     def set_info(self, info):
         """
         更新信息，
         """
         self.__info.update(info)  
     
+    # 使用key获取存储对象的某个属性
     def get_info(self, key):
         if key in self.__info:
             return self.__info[key]     
         else:
             return False 
 
+    # 存储对象信息处理
     def deal(self):
         mongo = MongoDB(MONGODB_URI, "notices")
         mongo.insert(self.__info)
