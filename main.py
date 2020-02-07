e9296d73987018507bd39fe4b9476d069cb0a5d3
lureiny <lureniy@gmail.com>
2020-02-07 16:51:08 +0800

    feat(main.py): 对爬虫数量进行判断

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import time
 if __name__ == '__main__':
     Spiders = __import__("spider")
     while True:
+        if len(SPIDERS) == 0:
+            print("未指定爬虫，程序退出")
+            break
         for spider in SPIDERS:
             spider_class = getattr(Spiders, spider)
             spider = spider_class()
-            spider.spider(
+            spider.spider()
         time.sleep(SLEEP_TIME)
-    
-
