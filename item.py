f7aad97479e94275d5252e51b8af952759851637
lureiny <lureniy@gmail.com>
2020-02-07 15:52:25 +0800

    refactor(item.py): 优化代码

--- item.py
+++ item.py
@@ -6,32 +6,42 @@ class Item():
     # 定义字段名
     def __init__(self):
         super().__init__()
-        self.__info = dict()        # Item的信息
+        self._info = dict()        # Item的信息
     
     # 更新存储对象类信息
     def set_info(self, info):
         """
         更新信息，
         """
-        self.__info.update(info)  
+        self._info.update(info)  
     
     # 使用key获取存储对象的某个属性
     def get_info(self, key):
-        if key in self.__info:
-            return self.__info[key]     
+        if key in self._info:
+            return self._info[key]     
         else:
             return False 
 
     # 存储对象信息处理
     def deal(self):
         mongo = MongoDB(MONGODB_URI, "notices")
-        mongo.insert(self.__info)
+        mongo.insert(self._info)
 
     def __str__(self):
-        return str(self.__info)
+        return str(self._info)
 
 
 class GDWJWItem(Item):
     def __init__(self):
         super().__init__()
+        self._info = {
+            "_id": "",
+            "title": "",
+            "sourceUrl": "",
+            "agency": "",
+            "date": "",
+            "source": "",
+            "body": [],
+            "effective": False
+        }
         
